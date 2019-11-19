e6fca7c4239cb8a2f8ab20da037272dbb5e823ce
maxlataci <56902232+maxlataci@users.noreply.github.com>
2019-11-18 23:00:25 -0300

    Projeto #1 - questão #4

--- maxpy.py
+++ maxpy.py
@@ -1,19 +1,21 @@
-import pandas as pd
-import numpy as np
-
 def show_df(df):
     print("O dataframe agora possui", df.shape[0], "linhas e", df.shape[1], "colunas.")
+    print(df.info())
     print(df.count())
     print(df.columns)
     print(df.dtypes)
     print(df.describe())
+    print(df.hist(bins=50, figsize=(20, 15)))
+    return
 
 def unique_values(df, coluna):
-    print('Valores distintos de', coluna, pd.(df[coluna]))
+    print("Valores distintos de", coluna, df[coluna].unique())
+    return
 
-def drop_columns(df, coluna):
-    df.drop(coluna, axis=1)
-    return df
+def drop_columns(df, colunas):
+    for coluna in colunas:
+        df.drop(coluna, axis=1, inplace=True)
+    return
 
 def create_subset(arquivo_origem, limite):
     # cria arquivo destino com 'limite' linhas a partir do arquivo origem
