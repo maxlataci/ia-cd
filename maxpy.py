a59508c44df19f5e9557aa6d4135637b2694d164
maxlataci <56902232+maxlataci@users.noreply.github.com>
2019-12-01 11:38:02 -0300

    Projeto #1 - questão #4

--- maxpy.py
+++ maxpy.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def show_df(df):
     print("O dataframe agora possui", df.shape[0], "linhas e", df.shape[1], "colunas.")
     print(df.info())
@@ -8,8 +10,33 @@ def show_df(df):
     print(df.hist(bins=50, figsize=(20, 15)))
     return
 
+def correlacao(df, campo1, campo2):
+    print(df[campo1].corr(df[campo2]))
+
+def agrupa_estados_regiao(df, campo_estado, campo_regiao):
+    regiao_norte = ['AC', 'AM', 'AP', 'PA', 'RO', 'RR', 'TO']
+    regiao_nordeste = ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'RN', 'SE']
+    regiao_centrooeste = ['DF', 'GO', 'MS', 'MT']
+    regiao_sudeste =['ES', 'MG', 'RJ', 'SP']
+    regiao_sul = ('PR', 'RS', 'SC')
+
+    df[campo_regiao] = 'NaN'
+    for index, row in df.iterrows():
+        if df.loc[index, campo_estado] in regiao_sul:
+            df.loc[index, campo_regiao] = "S"
+        elif df.loc[index, campo_estado] in regiao_centrooeste:
+            df.loc[index, campo_regiao] = "CO"
+        elif df.loc[index, campo_estado] in regiao_sudeste:
+            df.loc[index, campo_regiao] = "SE"
+        elif df.loc[index, campo_estado] in regiao_norte:
+            df.loc[index, campo_regiao] = "N"
+        else:
+            df.loc[index, campo_regiao] = "NE"
+    unique_values(df, campo_regiao)
+
 def unique_values(df, coluna):
     print("Valores distintos de", coluna, df[coluna].unique())
+    print(pd.value_counts(df[coluna].values, sort=True), "\n")
     return
 
 def drop_columns(df, colunas):
